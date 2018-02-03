ee959ae8bb2aa7c4a04a70e3d7224e56cb6619e9
Abdul Dakkak <abduld@wolfram.com>
2018-02-03 01:02:29 -0600

    vendor package

--- uuid.go
+++ uuid.go
@@ -5,24 +5,22 @@ import (
 	"encoding/hex"
 
 	"github.com/davecgh/go-spew/spew"
-
-	gouuid "github.com/satori/go.uuid"
 )
 
 func NewV4() string {
-	u4 := gouuid.Must(gouuid.NewV4())
+	u4 := Must(global.NewV4())
 	return u4.String()
 }
 
 func NewV5(u, name string) string {
-	u5 := gouuid.NewV5(gouuid.FromStringOrNil(u), name)
+	u5 := global.NewV5(FromStringOrNil(u), name)
 	return u5.String()
 }
 
 func New(obj interface{}) string {
-	u4 := gouuid.Must(gouuid.NewV4())
+	u4 := Must(global.NewV4())
 	h := sha1.New()
 	spew.Fdump(h, obj)
-	u5 := gouuid.NewV5(u4, hex.EncodeToString(h.Sum(nil)))
+	u5 := global.NewV5(u4, hex.EncodeToString(h.Sum(nil)))
 	return u5.String()
 }
